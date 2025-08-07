afb78352655ffd23e86fe07523071ee497414d3f
tahaofqirhamma <taha_ofqirhamma@um5.ac.ma>
2025-08-07 22:04:50 +0100

    prisma configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1 className="text-4xl font-bold">Welcome to Your Next.js App</h1>
-        <p className="text-lg mt-4">Start building your project here!</p>
-      </div>
-    </main>
+    <h1>Hello World</h1>
   );
 }
